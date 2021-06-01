f2e98ab05808eb5a1b1bb075b580cf9e163ad0a6
Artur Lazarev <art-king777@yandex.ru>
2021-06-01 16:26:50 +0500

    Проетная работа 4 изменил функции

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,33 @@
 let openPopupButton = document.querySelector('.profile__open-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__button-close');
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-  formElement.addEventListener('submit', formSubmitHandler); 
-}
-openPopupButton.addEventListener("click", openPopup); 
-
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-closePopupButton.addEventListener('click', closePopup);
-
-
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_job');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
+function openPopup() {
+  popup.classList.add('popup_opened');
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileJob .textContent = jobInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
 }
 
+openPopupButton.addEventListener("click", openPopup); 
+
+closePopupButton.addEventListener('click', closePopup);
+
+formElement.addEventListener('submit', formSubmitHandler); 
 
 
